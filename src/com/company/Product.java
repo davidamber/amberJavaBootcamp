e7ce1cf16ab326c42a7f52f2a9eda886659e5178
davidamber <david575@umn.edu>
2022-03-04 16:55:02 -0600

    Pushing Cafe Project

---
+++ src/com/company/Product.java
@@ -0,0 +1,59 @@
+package com.company;
+
+
+public class Product {
+    private String name;
+    private double price;
+    private String description;
+    private int quantity;
+
+    public Product() {
+        this.name = "";
+        this.price = 0.00;
+        this.description = "";
+        this.quantity = 0;
+    }
+
+    public Product(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.quantity = 0;
+    }
+
+    // getters and setters
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+    public void setPrice(double newPrice) {
+        this.price = newPrice;
+    }
+    public void setDescription(String newDescription) {
+        this.description = newDescription;
+    }
+    public void setQuantity(int newQuantity) {
+        this.quantity = newQuantity;
+    }
+
+    public double calculateProductTotal() {
+        // calculate product subtotal for the order (price * quantity)
+        return this.price * this.quantity;
+        }
+
+
+        }
+
+
