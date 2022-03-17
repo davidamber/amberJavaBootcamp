e0014dddc0c8842e355dd93f721ee3cbefc93027
amberdavidson <david575@umn.edu>
2022-03-16 19:21:46 -0500

    HTML projects

---
+++ src/main/java/com/amberJavaBootcamp/InClassExamples/HQL/FindUser_Hql.java
@@ -0,0 +1,39 @@
+package com.amberJavaBootcamp.InClassExamples.HQL;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import com.test.hib.model.User;
+
+import javax.persistence.TypedQuery;
+
+class App {
+    public static void main( String[] args ) {
+        FindUser_Hql u = new FindUser_Hql();
+        u.findUser();
+    }
+}
+
+public class FindUser_Hql {
+
+    public void findUser() {
+        // TODO Auto-generated method stub
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+
+        String hql = "FROM User";
+        TypedQuery query = session.createQuery(hql);
+        List<User> results = query.getResultList();
+
+        for (User u : results) {
+            System.out.println("User Id: " + u.getId() + "|"
+                    + " Full name: " + u.getFullname() +" | "
+                    + " Email: " + u.getEmail() +" | "
+                    + " password " + u.getPassword());
+        }
+    }
+}
+
---
+++ src/main/java/com/amberJavaBootcamp/InClassExamples/HibernateBasics/Student.java
@@ -0,0 +1,19 @@
+package com.amberJavaBootcamp.InClassExamples.HibernateBasics;
+
+
+
+public class Student {
+    private Integer id;
+    private String fname, lname;
+
+    Session session = factory.buildSession();
+}
+
+
+// Create a domain class of your choice
+//        (Student or Department class) and perform the following
+//        operations:
+//        - Create
+//        - Find (Read)
+//        - Update
+//        - Delete
